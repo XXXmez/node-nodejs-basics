25d11b9a84584baa3e838890275251be02016d10
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-06-05 15:02:35 +0300

    init: in the process

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,20 @@
+import fs from "fs";
+
+const filePath = "src/fs/files";
+const filePathCopy = "src/fs/files_copy";
+
 export const copy = async () => {
-    // Write your code here 
+    fs.access(filePath, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.access(filePathCopy, (error) => {
+                if (error) {
+                    //fs.copy 
+                }
+            })
+        }
+    })
 };
+
+copy();
