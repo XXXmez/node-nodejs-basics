cc92c47e567251ddd1703919c0610e78fd1b61d2
Ruslan Bogdanov <russian.bogd@gmail.com>
2022-06-05 15:02:15 +0300

    done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import fs from "fs";
+
+const filePath = "src/fs/files/fresh.txt";
+const text = "I am fresh and young";
+
 export const create = async () => {
-    // Write your code here 
+  fs.access(filePath, (error) => {
+    if (error) {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) throw new Error(err);
+      });
+    } else {
+      throw new Error('FS operation failed');
+    }
+  });
 };
+
+create();
